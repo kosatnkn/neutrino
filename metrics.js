3bf50ee3921360d441a20590633390b261a4251c
Kosala Tennakoon <kosala.me@gmail.com>
2019-12-06 11:15:55 +0530

    refined cluster mode

---
+++ metrics.js
@@ -0,0 +1,65 @@
+/*
+* Create dedicated HTTP server to expose application metrics.
+*/
+module.exports = function(config)
+{
+    const express = require('express');
+    const logger = require('./app/logger');
+
+    let metricsServer = express();
+
+    /**
+     * Start a metric server for a single threaded service.
+     */
+    function startMetricServer()
+    {
+        const { getContentType, getSummary } = require('@promster/express');
+
+        metricsServer.get(config.path, function(req, res)
+        {
+            res.statusCode = 200;
+
+            res.setHeader('Content-Type', getContentType());
+            res.end(getSummary());
+        });
+
+        metricsServer.listen(config.port);
+
+        let metrics = `Metrics are exposed on port ${config.port} \nMetric path is '${config.path}'`;
+        logger.prompt(metrics);
+    }
+
+    /**
+     * Start a metric server for clustered service.
+     */
+    function startClusterMetricServer()
+    {
+        const AggregatorRegistry = require('prom-client').AggregatorRegistry;
+            
+        let aggregatorRegistry = new AggregatorRegistry();
+
+        metricsServer.get(config.path, function(req, res)
+        {
+            aggregatorRegistry.clusterMetrics(function(err, metrics)
+            {
+                if(err)
+                {
+                    logger.error(err);
+                }
+
+                res.set('Content-Type', aggregatorRegistry.contentType);
+                res.send(metrics);
+            });
+        });
+
+        metricsServer.listen(config.port);
+
+        let metrics = `Metrics are exposed on port ${config.port} \nMetric path is '${config.path}'`;
+        logger.prompt(metrics);
+    }
+
+    return {
+        startMetricServer: startMetricServer,
+        startClusterMetricServer: startClusterMetricServer
+    };
+}
--- service.js
+++ service.js
@@ -8,29 +8,45 @@
  |
  */
 
-const express = require('express');
 const os = require('os');
 const app = require('./app');
 const logger = require('./app/logger');
 
+let isMetricsEnabled = (process.env.APP_METRICS === 'true');
+let metrics = null;
+
+if(isMetricsEnabled)
+{
+    let metricConfig = {
+        port: process.env.APP_METRICS_PORT,
+        path: process.env.APP_METRICS_ROUTE || '/metrics'
+    };
+
+    metrics = require('./metrics')(metricConfig);
+}
+
 let isClusterMode = (process.env.APP_CLUSTER === 'true');
 
-// create dedicated HTTP server to expose application metrics
-let isMetricsEnabled = (process.env.APP_METRICS === 'true');
-let metricsPort = process.env.APP_METRICS_PORT;
-let metricsPath = process.env.APP_METRICS_ROUTE || '/metrics';
-let metricsServer = express();
 
-// create the toast message for application startup
-let toast = "name - "  + process.env.APP_NAME +
-    ", port - " + process.env.APP_PORT +
-    ", timezone - " + process.env.APP_TIMEZONE +
-    ", mode - " + (process.env.APP_DEBUG === 'true' ? "DEBUG" : "PRODUCTION") +
-    ", metrics_port - " + (process.env.APP_METRICS === 'true' ? metricsPort : "DISABLED") +
-    ", metrics_route - " + (process.env.APP_METRICS === 'true' ? metricsPath : "DISABLED");
+let splash = 
+` _   _            _        _             
+ | \ | |          | |      (_)            
+ |  \| | ___ _   _| |_ _ __ _ _ __   ___  
+ | .   |/ _ \ | | | __| '__| | '_ \ / _ \
+ | |\  |  __/ |_| | |_| |  | | | | | (_) |
+ |_| \_|\___|\__,_|\__|_|  |_|_| |_|\___/ 
+ `;
 
+let info = `
+Service started on port ${process.env.APP_PORT}
+Service timezone is '${process.env.APP_TIMEZONE}'
+Running on '${process.env.APP_DEBUG === 'true' ? "DEBUG" : "PRODUCTION"}' mode`;
 
 // start the application in cluster mode
+// NOTE: The cluster mode's fork will create a new thread and initialize a 
+// new instance of the app in that thread. Which means every fork will run 
+// this entire script from beginning to end. That is why the cluster mode
+// `if(isClusterMode)` block and `if(cluster.isMaster)` have returns.
 if(isClusterMode)
 {
     const cluster = require('cluster');
@@ -40,15 +56,9 @@ if(isClusterMode)
 
     if(cluster.isMaster)
     {
-        // add additional data to toast for the master instance
-        let toastMaster = toast + ", cluster - ENABLED" +
-                            ", cluster_workers - " + numCPUs +
-                            ", cluster_process - MASTER" +
-                            ", cluster_process_status - STARTED" +
-                            ", cluster_process_id - " + process.pid;
-
-        logger.info(toastMaster);
-
+        logger.prompt(splash);
+        logger.prompt(info);
+        
         // fork workers
         for (let i = 0; i < numCPUs; i++)
         {
@@ -58,83 +68,48 @@ if(isClusterMode)
         // handle exited worker processes
         cluster.on('exit', function(worker, code, signal)
         {
-            // add additional data to toast for worker instance
-            let toastWorker = toast + ", cluster - ENABLED" +
-                                ", cluster_workers - " + numCPUs +
-                                ", cluster_process - WORKER" +
-                                ", cluster_process_status - TERMINATED" +
-                                ", cluster_process_id - " + process.pid;
-
-
-            logger.error(toastWorker);
+            logger.error(`Terminated WORKER, process ID: ${process.pid}`);
 
             // replace the terminated worker
             cluster.fork();
         });
 
-
         // export cluster metrics
         if(isMetricsEnabled)
         {
-            const AggregatorRegistry = require('prom-client').AggregatorRegistry;
-            
-            let aggregatorRegistry = new AggregatorRegistry();
-
-            metricsServer.get(metricsPath, function(req, res)
-            {
-                aggregatorRegistry.clusterMetrics(function(err, metrics)
-                {
-                    if(err)
-                    {
-                        logger.error(err);
-                    }
-
-                    res.set('Content-Type', aggregatorRegistry.contentType);
-                    res.send(metrics);
-                });
-            });
-
-            metricsServer.listen(metricsPort);
+            metrics.startClusterMetricServer();
         }
+
+        logger.prompt("Cluster mode ENABLED");
+        logger.prompt(`Using ${numCPUs} CPUs`);
+        logger.info(`Started MASTER, process ID: ${process.pid}`);
 
         return;
     }
-
-    // add additional data to toast for worker instance
-    let toastWorker = toast + ", cluster - ENABLED" +
-                        ", cluster_workers - " + numCPUs +
-                        ", cluster_process - WORKER" +
-                        ", cluster_process_status - STARTED" +
-                        ", cluster_process_id - " + process.pid;
 
     // workers can share any TCP connection
     // listen for requests in worker mode
     app.listen(process.env.APP_PORT, function ()
     {
-        logger.info(toastWorker);
+        logger.info(`Started WORKER, process ID: ${process.pid}`);
     });
 
     return;
 }
 
+// start application in single threaded mode
+
+logger.prompt(splash);
+logger.prompt(info);
+
+// export single worker metrics
 if(isMetricsEnabled)
 {
-    // export single worker metrics
-    const { getContentType, getSummary } = require('@promster/express');
-
-    metricsServer.get(metricsPath, function(req, res)
-    {
-        res.statusCode = 200;
-
-        res.setHeader('Content-Type', getContentType());
-        res.end(getSummary());
-    });
-
-    metricsServer.listen(metricsPort);
+    metrics.startMetricServer();
 }
 
 // Listen for requests
 app.listen(process.env.APP_PORT, function ()
 {
-    logger.info(toast);
+    logger.prompt(`Process ID: ${process.pid}`);
 });
